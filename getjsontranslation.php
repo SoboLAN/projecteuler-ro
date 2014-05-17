2870759204e838d3d940081063f32802f0d9eae2
Radu Murzea <radu.murzea@gmail.com>
2014-05-17 18:21:37 +0300

    More fixes to getjsontranslation.php

--- getjsontranslation.php
+++ getjsontranslation.php
@@ -1,15 +1,15 @@
 <?php
 
 //returns a DOMNode's content, without stripping internal HTML tags
-function DOMinnerHTML ($element)
+function DOMinnerHTML($element)
 {
     $innerHTML = "";
     $children = $element->childNodes;
         
     foreach ($children as $child) {
-        $tmp_dom = new DOMDocument ();
-        $tmp_dom->appendChild ($tmp_dom->importNode ($child, true));
-        $innerHTML .= trim ($tmp_dom->saveHTML ());
+        $tmp_dom = new DOMDocument();
+        $tmp_dom->appendChild($tmp_dom->importNode($child, true));
+        $innerHTML .= trim($tmp_dom->saveHTML());
     }
     
     return $innerHTML;
@@ -18,13 +18,13 @@ function DOMinnerHTML ($element)
 //returns the problem content from the specified URL.
 //it will be in raw HTML form.
 //if no "<div class="problem_content"..." element is found, "NONE" is returned
-function getProblemContent ($url)
+function getProblemContent($url)
 {
     //get all the html content
-    $html = file_get_contents ($url);
+    $html = file_get_contents($url);
         
     //no content retrieved...
-    if ($html === FALSE OR strlen ($html) < 40) {
+    if ($html === false OR strlen($html) < 40) {
         return 'NONE';
     }
         
@@ -33,29 +33,29 @@ function getProblemContent ($url)
     libxml_use_internal_errors(true);
     
     //the Document Object Model
-    $dom = new DOMDocument ();
+    $dom = new DOMDocument();
     
     //load the HTML page in the DOM
-    $dom->loadHTML ($html);
+    $dom->loadHTML($html);
     
     //the problem content is found in a div, so let's get all the div's
-    $list = $dom->getElementsByTagName ('div');
+    $list = $dom->getElementsByTagName('div');
     
     for ($i = 0; $i < $list->length; $i++) {
         //traversing all the div's
-        $item = $list->item ($i);
+        $item = $list->item($i);
             
         //attributes needed, since we're looking for "<div class="problem_content">"
         $attr = $item->attributes;
             
-        if ($attr !== NULL) {
-            $s = $attr->getNamedItem ('class');
+        if (! is_null($attr)) {
+            $s = $attr->getNamedItem('class');
                 
-            if ($s !== NULL AND $s->nodeValue == 'problem_content') {
+            if (! is_null($s) && $s->nodeValue == 'problem_content') {
                 //if the correct item is found, it needs to go through
                 //the DOMinnerHTML function. otherwise, the internal HTML code
                 //will be stripped when the content is extracted
-                return DOMinnerHTML ($item);
+                return DOMinnerHTML($item);
             }
         }
     }
@@ -64,45 +64,45 @@ function getProblemContent ($url)
     return 'NONE';
 }
     
-if (! isset ($_GET['problem']) || ! isset ($_GET['lang'])) {
-    exit (-1);
+if (! isset($_GET['problem']) || ! isset($_GET['lang'])) {
+    exit(-1);
 }
     
 $problemID = $_GET['problem'];
 $lang = $_GET['lang'];
     
-$available_languages = array ('ro', 'en', 'ru', 'es', 'kr');
+$available_languages = array('ro', 'en', 'ru', 'es', 'kr');
 
-if (in_array ($lang, $available_languages) === FALSE) {
-    exit (-3);
+if (in_array($lang, $available_languages) === FALSE) {
+    exit(-3);
 }
     
 //eliminate some junk... (people can put all sorts of stuff in this thing)...
-if (strlen ($problemID) > 4 || strpos ($problemID, '.') !== FALSE || strpos ($problemID, '/') !== FALSE) {
-    exit (-4);
+if (strlen($problemID) > 4 || strpos($problemID, '.') !== false || strpos($problemID, '/') !== false) {
+    exit(-4);
 }
-    
-require_once ('include/peproblem.class.php');
-    
+
+require_once 'include/peproblem.class.php';
+
 //create the problem object
-$problemObj = PEProblem::withID ($problemID);
-    
+$problemObj = PEProblem::withID($problemID);
+
 //if no such problem exists, return 'NONE'
-if ($problemObj === FALSE) {
+if ($problemObj === false) {
     $array = array ('translation' => 'NONE');
     
-    echo 'processTranslation (' . json_encode ($array) . ');';
+    echo 'processTranslation (' . json_encode($array) . ');';
     
-    exit (0);
+    exit(0);
 }
-    
+
 $theData = 'NONE';
 $query = 'NONE';
-    
+
 switch ($lang) {
     case 'ro':    //romanian
     {
-        $theData = $problemObj->isTranslated () ? $problemObj->getTextRO () : 'NONE';
+        $theData = $problemObj->isTranslated() ? $problemObj->getTextRO() : 'NONE';
         
         $query = 'UPDATE gmonkeyaccesses SET accesses_ro = accesses_ro + 1 WHERE problem_id = ' . $problemID;
             
@@ -110,7 +110,7 @@ switch ($lang) {
     }
     case 'en':    //english
     {
-        $theData = $problemObj->getTextEN ();
+        $theData = $problemObj->getTextEN();
         
         $query = 'UPDATE gmonkeyaccesses SET accesses_en = accesses_en + 1 WHERE problem_id = ' . $problemID;
         
@@ -118,7 +118,7 @@ switch ($lang) {
     }
     case 'ru':    //russian
     {
-        $theData = getProblemContent ('http://euler.jakumo.org/problems/view/' . $_GET['problem'] . '.html');
+        $theData = getProblemContent('http://euler.jakumo.org/problems/view/' . $_GET['problem'] . '.html');
         
         $query = 'UPDATE gmonkeyaccesses SET accesses_ru = accesses_ru + 1 WHERE problem_id = ' . $problemID;
         
@@ -126,7 +126,7 @@ switch ($lang) {
     }
     case 'es':    //spanish
     {
-        $theData = getProblemContent ('http://euleres.tk/problems.php?id=' . $_GET['problem']);
+        $theData = getProblemContent('http://euleres.tk/problems.php?id=' . $_GET['problem']);
         
         $query = 'UPDATE gmonkeyaccesses SET accesses_es = accesses_es + 1 WHERE problem_id = ' . $problemID;
         
@@ -134,24 +134,24 @@ switch ($lang) {
     }
     case 'kr':    //korean
     {
-        $theData = getProblemContent ('http://euler.synap.co.kr/prob_detail.php?id=' . $_GET['problem']);
+        $theData = getProblemContent('http://euler.synap.co.kr/prob_detail.php?id=' . $_GET['problem']);
         
         $query = 'UPDATE gmonkeyaccesses SET accesses_kr = accesses_kr + 1 WHERE problem_id = ' . $problemID;
         
         break;
     }
 }
-    
-if ($query != 'NONE' AND $theData != 'NONE') {
+
+if ($query !== 'NONE' && $theData !== 'NONE') {
     require_once ('include/db.class.php');
     
-    $db = new DBConn ();
+    $db = new DBConn();
     
-    $r = $db->executeQuery ($query);
+    $r = $db->executeQuery($query);
     
-    $db->closeConnection ();
+    $db->closeConnection();
 }
-    
-$array = array ('translation' => $theData);
 
-echo 'processTranslation (' . json_encode ($array) . ');';
+$array = array('translation' => $theData);
+
+echo 'processTranslation (' . json_encode($array) . ');';
