ff4202aa1090181240b5c52e85547af99d6d5bce
Radu Murzea <radu.murzea@gmail.com>
2014-05-17 19:42:43 +0300

    Refactored home page

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
+
 require_once 'autoload.php';
+
 use ProjectEuler\Main\Site;
 use ProjectEuler\Main\PEException;
 
 try {
     $site = new Site();
+    
+    $finalContent = file_get_contents('templates/index.tpl');
 
     $htmlout = $site->getFullPageTemplate('index.php');
-
+    
 } catch (PEException $ex) {
     switch ($ex->getType()) {
         case PEException::ERROR:
             header('Location: 500.shtml');
+			exit();
+            break;
+        case PEException::INVALID_REQUEST:
+            header('Location: 400.shtml');
 			exit();
             break;
         case PEException::SITE_OFFLINE:
@@ -24,40 +32,6 @@ try {
     }
 }
 
-$text = '<h2>Despre Project Euler @ RO</h2>
-
-<h3>Ce e Project Euler @ RO ?</h3>
-<p>Acest site conține o bază de date cu toate problemele de pe <a href="http://projecteuler.net/" target="blank">ProjectEuler.net</a> traduse în limba română.</p>
-<br />
-
-<h3>De ce ar fi nevoie de aceste probleme în limba română ?</h3>
-<p>La problemele de pe Project Euler muncesc o echipă profesionistă și foarte dedicată de programatori și matematicieni.
-Calitatea problemelor este, prin urmare, foarte ridicată. Tocmai din acest motiv, Project Euler este un site foarte
-popular la nivel mondial. Mulți oameni care nu au limba engleza ca limbă maternă ar vrea sa rezolve
-aceste probleme, inclusiv oameni din România. Traducerile de aici vor face ca aceste probleme să fie accesibile și lor.</p>
-<br />
-
-<h3>Am rezolvat problema. Cum aflu dacă am rezultatul corect ?</h3>
-<p>Pe pagina fiecarei probleme este un link către problema originală, de pe site-ul mamă. Intrați acolo și veți putea să verificați dacă ați rezolvat corect o
-anumită problemă.</p>
-<br />
-
-<h3>Nu pot vedea o problemă tradusă direct pe ProjectEuler.net ?</h3>
-<p>Ba da, se poate. Daca aveți browser-ul Mozilla Firefox instalat impreună cu extensia GreaseMonkey, atunci puteți să vă folosiți de un script care integrează
-traducerile direct pe site-ul mamă. In felul acesta, dacă sunteți pe ProjectEuler.net și vreți să vedeți o problemă tradusă, puteți face asta cu un singur click.
-Pentru instalarea script-ului (tutorial și un filmuleț demonstrativ), intrați pe pagina urmatoare: <a href="greasemonkey.php">Script GreaseMonkey</a>. (Un link către
-această pagină se află și în meniul de sus)</p>
-<br />
-
-<h3>Am găsit o eroare de traducere. Cum se poate repara ?</h3>
-<p>Daca gasiți o eroare de traducere puteți raporta acest lucru pe pagina de Contact.</p>
-
-
-<div class="clear"></div>
-<br />
-
-<div class="clear"></div>';
-
-$htmlout = str_replace('{page_content}', $text, $htmlout);
+$htmlout = str_replace('{page_content}', $finalContent, $htmlout);
 
 echo $htmlout;
---
+++ templates/index.tpl
@@ -0,0 +1,27 @@
+<h2>Despre Project Euler @ RO</h2>
+
+<h3>Ce e Project Euler @ RO ?</h3>
+<p>Acest site conține o bază de date cu toate problemele de pe <a href="http://projecteuler.net/" target="blank">ProjectEuler.net</a> traduse în limba română.</p>
+<br />
+
+<h3>De ce ar fi nevoie de aceste probleme în limba română ?</h3>
+<p>La problemele de pe Project Euler muncesc o echipă profesionistă și foarte dedicată de programatori și matematicieni.
+Calitatea problemelor este, prin urmare, foarte ridicată. Tocmai din acest motiv, Project Euler este un site foarte
+popular la nivel mondial. Mulți oameni care nu au limba engleza ca limbă maternă ar vrea sa rezolve
+aceste probleme, inclusiv oameni din România. Traducerile de aici vor face ca aceste probleme să fie accesibile și lor.</p>
+<br />
+
+<h3>Am rezolvat problema. Cum aflu dacă am rezultatul corect ?</h3>
+<p>Pe pagina fiecarei probleme este un link către problema originală, de pe site-ul mamă. Intrați acolo și veți putea să verificați dacă ați rezolvat corect o
+anumită problemă.</p>
+<br />
+
+<h3>Nu pot vedea o problemă tradusă direct pe ProjectEuler.net ?</h3>
+<p>Ba da, se poate. Daca aveți browser-ul Mozilla Firefox instalat impreună cu extensia GreaseMonkey, atunci puteți să vă folosiți de un script care integrează
+traducerile direct pe site-ul mamă. In felul acesta, dacă sunteți pe ProjectEuler.net și vreți să vedeți o problemă tradusă, puteți face asta cu un singur click.
+Pentru instalarea script-ului (tutorial și un filmuleț demonstrativ), intrați pe pagina urmatoare: <a href="greasemonkey.php">Script GreaseMonkey</a>. (Un link către
+această pagină se află și în meniul de sus)</p>
+<br />
+
+<h3>Am găsit o eroare de traducere. Cum se poate repara ?</h3>
+<p>Daca gasiți o eroare de traducere puteți raporta acest lucru pe pagina de Contact.</p>
