337a59a9470abb0a4d3bb845144878f4c550ccc1
Radu Murzea <radu.murzea@gmail.com>
2014-05-17 13:45:19 +0300

    Some fixes to the Database class

--- src/ProjectEuler/Main/Database.php
+++ src/ProjectEuler/Main/Database.php
@@ -5,6 +5,9 @@ namespace ProjectEuler\Main;
 use FileListPoker\Main\PEException;
 use FileListPoker\Main\Logger;
 
+use PDO as PDO;
+use PDOException as PDOException;
+
 /**
  * Handles the construction of a PDO object capable of performing database operations.
  * @author Radu Murzea <radu.murzea@gmail.com>
@@ -33,7 +36,7 @@ class Database
     private static function buildConnection()
     {
         if (! is_readable(self::$configPath)) {
-            $ex = new PEException('config file is inaccessible', FLPokerException::ERROR);
+            $ex = new PEException('config file is inaccessible', PEException::ERROR);
             Logger::log($ex->getMessage());
             throw $ex;
         }
@@ -42,7 +45,7 @@ class Database
         $dbConfig = json_decode(file_get_contents(self::$configPath), true);
         
         if (is_null($dbConfig)) {
-             $ex = new PEException('config file is corrupt', FLPokerException::ERROR);
+             $ex = new PEException('config file is corrupt', PEException::ERROR);
              Logger::log($ex->getMessage());
              throw $ex;
         }
@@ -57,13 +60,13 @@ class Database
 
         // connection options I like
         $options = array (
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         );
 
         // connection charset handling for old php versions
         if ($dsnarr['charset'] and version_compare(PHP_VERSION, '5.3.6', '<')) {
-            $options[\PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES '.$dsnarr['charset'];
+            $options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES '.$dsnarr['charset'];
         }
         $dsnpairs = array();
         foreach ($dsnarr as $k => $v) {
@@ -74,15 +77,15 @@ class Database
 
         try {
             $dsn = 'mysql:' . implode(';', $dsnpairs);
-            self::$connection = new \PDO($dsn, $dbConfig['user'], $dbConfig['pass'], $options);
+            self::$connection = new PDO($dsn, $dbConfig['user'], $dbConfig['pass'], $options);
 
             //set prepared statement emulation depending on server version
-            $serverversion = self::$connection->getAttribute(\PDO::ATTR_SERVER_VERSION);
+            $serverversion = self::$connection->getAttribute(PDO::ATTR_SERVER_VERSION);
             $emulate_prepares = (version_compare($serverversion, $emulate_prepares_below_version, '<'));
-            self::$connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, $emulate_prepares);
-        } catch (\PDOException $e) {
+            self::$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, $emulate_prepares);
+        } catch (PDOException $ex) {
             $message = 'There was an error while connecting to the database';
-            $ex = new PEException($message, FLPokerException::ERROR);
+            $ex = new PEException($message, PEException::ERROR);
             Logger::log("$message: " . $ex->getMessage());
             throw $ex;
         }
