1aad7ea0bd3de97325a0f7f697452b02f6c8bcb4
Radu Murzea <radu.murzea@gmail.com>
2014-05-17 16:11:21 +0300

    Refactored the copyright page

--- copyright.php
+++ copyright.php
@@ -1,32 +1,37 @@
 <?php
-    require ('header.php');
-?>
 
-<div id="content">
+require_once 'autoload.php';
+
+use ProjectEuler\Main\Site;
+use ProjectEuler\Main\PEException;
+
+try {
+    $site = new Site();
     
-    <h2>Copyright</h2>
-    <br />
-    <h3>Numele Autorului: Murzea Radu</h3>
-    <br />
-    O parte a conținutului site-ului în limba engleză ProjectEuler.net este tradus în română aici.
-    <br /><br />
-    Copyright-ul original poate fi găsit <a href="http://projecteuler.net/copyright">aici</a>.
-    <br /><br />
-    Conținutul de aici intră sub incidența licenței <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/">Creative Commons License: Attribution-NonCommercial-ShareAlike 2.0 UK: England &amp; Wales</a>.
-    <br /><br />
-    Sumarul acestei licențe este următorul:
-    <br />
-    <ul>
-        <li>Atribuire - obligația de a atribui această lucrare autorului original</li>
-        <li>Non-Comercial - interzicerea folosirii acestor materiale în scopuri comerciale</li>
-        <li>Distribuire Protejată - obligația de a distribui sub aceeași licență orice material derivat sau bazat pe acesta.</li>
-    </ul>
-    <br />
-    Pentru mai multe informații, vedeți <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/legalcode">codul legal al licenței</a>.
+    $finalContent = file_get_contents('templates/copyright.tpl');
     
-</div>
+    $htmlout = $site->getFullPageTemplate('index.php');
+    
+} catch (PEException $ex) {
+    switch ($ex->getType()) {
+        case PEException::ERROR:
+            header('Location: 500.shtml');
+			exit();
+            break;
+        case PEException::INVALID_REQUEST:
+            header('Location: 400.shtml');
+			exit();
+            break;
+        case PEException::SITE_OFFLINE:
+            header('Location: maintenance.shtml');
+			exit();
+            break;
+        default:
+            header('Location: 500.shtml');
+			exit();
+    }
+}
 
-</div>
+$htmlout = str_replace('{page_content}', $finalContent, $htmlout);
 
-</body>
-</html>
+echo $htmlout;    
---
+++ templates/copyright.tpl
@@ -0,0 +1,19 @@
+<h2>Copyright</h2>
+<br />
+<h3>Numele Autorului: Murzea Radu</h3>
+<br />
+O parte a conținutului site-ului în limba engleză ProjectEuler.net este tradus în română aici.
+<br /><br />
+Copyright-ul original poate fi găsit <a href="http://projecteuler.net/copyright">aici</a>.
+<br /><br />
+Conținutul de aici intră sub incidența licenței <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/">Creative Commons License: Attribution-NonCommercial-ShareAlike 2.0 UK: England &amp; Wales</a>.
+<br /><br />
+Sumarul acestei licențe este următorul:
+<br />
+<ul>
+    <li>Atribuire - obligația de a atribui această lucrare autorului original</li>
+    <li>Non-Comercial - interzicerea folosirii acestor materiale în scopuri comerciale</li>
+    <li>Distribuire Protejată - obligația de a distribui sub aceeași licență orice material derivat sau bazat pe acesta.</li>
+</ul>
+<br />
+Pentru mai multe informații, vedeți <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/legalcode">codul legal al licenței</a>.
